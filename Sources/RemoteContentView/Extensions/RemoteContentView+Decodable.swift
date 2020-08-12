8733f0e4ab1fb371d0b60afe36eb3368db08ccd4
Dmytro Anokhin <5136301+dmytro-anokhin@users.noreply.github.com>
2020-08-12 13:25:12 +0200

    Extension for all combination of generic view arguments

--- Sources/RemoteContentView/Extensions/RemoteContentView+Decodable.swift
+++ Sources/RemoteContentView/Extensions/RemoteContentView+Decodable.swift
@@ -35,6 +35,67 @@ public extension RemoteContentView where T: Decodable {
 
 
 @available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
+public extension RemoteContentView where T: Decodable, Empty == EmptyView {
+
+    init(urlSession: URLSession = .shared,
+         url: URL,
+         type: T.Type,
+         progress: @escaping () -> Progress,
+         failure: @escaping (_ message: String) -> Failure,
+         content: @escaping (_ value: T) -> Content)
+    {
+        self.init(urlSession: urlSession,
+                  url: url,
+                  type: type,
+                  empty: { EmptyView() },
+                  progress: progress,
+                  failure: failure,
+                  content: content)
+    }
+}
+
+
+@available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
+public extension RemoteContentView where T: Decodable, Empty == EmptyView, Progress == ActivityIndicator {
+
+    init(urlSession: URLSession = .shared,
+         url: URL,
+         type: T.Type,
+         failure: @escaping (_ message: String) -> Failure,
+         content: @escaping (_ value: T) -> Content)
+    {
+        self.init(urlSession: urlSession,
+                  url: url,
+                  type: type,
+                  empty: { EmptyView() },
+                  progress: { ActivityIndicator() },
+                  failure: failure,
+                  content: content)
+    }
+}
+
+
+@available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
+public extension RemoteContentView where T: Decodable, Empty == EmptyView, Failure == Text {
+
+    init(urlSession: URLSession = .shared,
+         url: URL,
+         type: T.Type,
+         progress: @escaping () -> Progress,
+         content: @escaping (_ value: T) -> Content)
+    {
+        self.init(urlSession: urlSession,
+                  url: url,
+                  type: type,
+                  empty: { EmptyView() },
+                  progress: progress,
+                  failure: { Text($0) },
+                  content: content)
+    }
+}
+
+
+@available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
 public extension RemoteContentView where T: Decodable, Empty == EmptyView, Progress == ActivityIndicator, Failure == Text {
 
     init(urlSession: URLSession = .shared,
--- Sources/RemoteContentView/Extensions/RemoteContentView+Extensions.swift
+++ Sources/RemoteContentView/Extensions/RemoteContentView+Extensions.swift
@@ -10,6 +10,67 @@ import SwiftUI
 
 
 @available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
+public extension RemoteContentView where T: Decodable, Empty == EmptyView {
+
+    init(urlSession: URLSession = .shared,
+         url: URL,
+         decode: @escaping (_ data: Data) throws -> T,
+         progress: @escaping () -> Progress,
+         failure: @escaping (_ message: String) -> Failure,
+         content: @escaping (_ value: T) -> Content)
+    {
+        self.init(urlSession: urlSession,
+                  url: url,
+                  decode: decode,
+                  empty: { EmptyView() },
+                  progress: progress,
+                  failure: failure,
+                  content: content)
+    }
+}
+
+
+@available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
+public extension RemoteContentView where T: Decodable, Empty == EmptyView, Progress == ActivityIndicator {
+
+    init(urlSession: URLSession = .shared,
+         url: URL,
+         decode: @escaping (_ data: Data) throws -> T,
+         failure: @escaping (_ message: String) -> Failure,
+         content: @escaping (_ value: T) -> Content)
+    {
+        self.init(urlSession: urlSession,
+                  url: url,
+                  decode: decode,
+                  empty: { EmptyView() },
+                  progress: { ActivityIndicator() },
+                  failure: failure,
+                  content: content)
+    }
+}
+
+
+@available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
+public extension RemoteContentView where T: Decodable, Empty == EmptyView, Failure == Text {
+
+    init(urlSession: URLSession = .shared,
+         url: URL,
+         decode: @escaping (_ data: Data) throws -> T,
+         progress: @escaping () -> Progress,
+         content: @escaping (_ value: T) -> Content)
+    {
+        self.init(urlSession: urlSession,
+                  url: url,
+                  decode: decode,
+                  empty: { EmptyView() },
+                  progress: progress,
+                  failure: { Text($0) },
+                  content: content)
+    }
+}
+
+
+@available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
 public extension RemoteContentView where T: Decodable, Empty == EmptyView, Progress == ActivityIndicator, Failure == Text {
 
     init(urlSession: URLSession = .shared,
